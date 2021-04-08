9d9a7e44409ae0b82dc92309a752af33c0e22c7a
rmzntlgy <ramazantolgay@hotmail.com>
2021-04-09 01:30:01 +0300

    Ders güncellemesi 9 Nisan 2021

--- StringFormat.py
+++ StringFormat.py
@@ -8,3 +8,10 @@ print("Merhaba, ben {1} {0}".format(isim, soyisim))
 print("Merhaba, ben {s} {i}".format(i=isim, s=soyisim))
 yas = 26
 print("Merhaba, ben {s} {i}, {y} yaşındayım.".format(i=isim, s=soyisim, y=yas))
+
+bolum= 13/7
+print("SONUÇ:{}".format(bolum))
+
+print("SONUÇ:{b:2.4}".format(b=bolum))
+
+print(f"Merhaba benim adım {isim} ve {yas} yaşındayım.") #f ile yazdırma işlemi şekildeki gibidir. En başa f yazdık.
