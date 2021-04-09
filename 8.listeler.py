c65ed5a2276557af365b8d487ba067a77dae8878
rmzntlgy <ramazantolgay@hotmail.com>
2021-04-09 04:21:10 +0300

    Ders güncellemesi 9 Nisan 2021

--- 8.listeler.py
+++ 8.listeler.py
@@ -5,5 +5,7 @@ sebzeler = ["salatalık", "patlıcan", "domates"]
 print(f"Buradaki iki listeyi birbirleriyle toplayabiliriz;{meyveler+sebzeler}")
 print("\nDaha önceki konulardaki bütün özellikleri kullanarak işlem yapabiliriz.")
 
-ic_ice = ["Ramazan", 26,["ACIPAYAM", "DENİZLİ"]]
+ic_ice = ["Ramazan", 26, ["ACIPAYAM", "DENİZLİ"]]
 print(f"\nBu örnek bir iç içe liste örneğidir;{ic_ice}")
+
+print("liste metotlarını; https://www.w3schools.com/python/python_ref_list.asp üzerinden bulabilirsiniz.")
