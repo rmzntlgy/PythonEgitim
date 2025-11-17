643f54f2a7de3cb523cc091eee840c9a011f4ad1
RAMAZAN TOLGAY <ramazantolgay@hotmail.com>
2025-11-17 23:08:08 +0100

    Delete #BONUS_COVİD_19_TR directory

--- #BONUS_COVİD_19_TR/Corona_vaka.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-import time
-driver = webdriver.Chrome()
-
-url = "https://covid19.saglik.gov.tr/TR-66935/genel-koronavirus-tablosu.html"
-driver.get(url)
-time.sleep(2)
